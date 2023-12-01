4e06d22034785fc7f3cd9bf018f7c389b39d8f11
Vincent Paradis <vipar39@ulaval.ca>
2023-12-01 10:44:42 -0500

    Contours values fix
    detect_contours() now returns contour values with the proper coordinate based on the original image and not the resized image.

--- src/contour_detection.py
+++ src/contour_detection.py
@@ -141,46 +141,24 @@ def detect_contours(image: np.ndarray, **kwargs) -> np.ndarray:
     :param img: Image to detect contours in.
     :return: Nx2 numpy array of points on the contour in the image.
     """
-    # load the image and compute the ratio of the old height
-    # to the new height, clone it, and resize it
-    ratio: float = image.shape[0] / 500.0
+    image_height_resize: int = 1500
+    
     orig: np.ndarray = image.copy()
-    image: np.ndarray = imutils.resize(image, height = 1500)
+    image: np.ndarray = imutils.resize(image, height = image_height_resize)
     image = cv2.bilateralFilter(image, 13, 200, 200)
     output_contour: np.ndarray = None
     area_img: int = image.shape[0] * image.shape[1]
-    # convert the image to grayscale, blur it, and find edges in the image
+    
     gray: np.ndarray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # gray = cv2.GaussianBlur(gray, (7, 7), 0)
-    # edged = cv2.Canny(gray, 75, 200, apertureSize=3, L2gradient=True) # kinda good
-
-
-    # edged2: np.ndarray = cv2.Canny(gray, 70, 150, apertureSize=5, L2gradient=True)
-
-    # edged = cv2.bitwise_or(edged2, edged)
-    print("STEP 1: Edge Detection")
+    
+    # print("STEP 1: Edge Detection")
     for tresh in range(120, 59, -15):
-        output: np.ndarray = image.copy()
+        output: np.ndarray = orig.copy()
         found: bool = False
         edged = cv2.Canny(gray, tresh, tresh + 10, apertureSize=5, L2gradient=False)
-        # show the original image and the edge detected image
-        # cv2.imwrite('./src./images/Image.png', image)
-        # cv2.imwrite('./src./images/gray.png', gray)
-        # _, edged = cv2.threshold(edged, 127, 255, cv2.THRESH_BINARY)
         edged = cv2.dilate(edged, kernel=None, anchor=(-1, -1), iterations=2, borderType=cv2.BORDER_CONSTANT, borderValue=(-1))
-        # edged = cv2.medianBlur(edged, 3)
-        # edged = cv2.GaussianBlur(edged, (3, 3), 0)
-
-        #edged = cv2.erode(edged, kernel=None, anchor=(-1, -1), iterations=2, borderType=cv2.BORDER_CONSTANT, borderValue=(-1))
-        #edged = cv2.dilate(edged, kernel=None, anchor=(-1, -1), iterations=2, borderType=cv2.BORDER_CONSTANT, borderValue=(-1))
-
-        #edged = cv2.dilate(edged, kernel=None, anchor=(-1, -1), iterations=2, borderType=cv2.BORDER_CONSTANT, borderValue=(-1))
-        #edged = cv2.erode(edged, kernel=None, anchor=(-1, -1), iterations=2, borderType=cv2.BORDER_CONSTANT, borderValue=(-1))
         edged = remove_edge(edged)
-        # cv2.imwrite('./src./images/Edged.png', edged)
 
-        # find the contours in the edged image, keeping only the
-        # largest ones, and initialize the screen contour
         contours: tuple = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
         contours: tuple = imutils.grab_contours(contours)
@@ -197,7 +175,7 @@ def detect_contours(image: np.ndarray, **kwargs) -> np.ndarray:
             filter_contours_max_vertices: float = 1000000.0
             filter_contours_min_vertices: float = 0.0
             filter_contours_min_ratio: float = 0.0
-            filter_contours_max_ratio: float = 6
+            filter_contours_max_ratio: float = 1000
             cnts = filter_contours(cnts,
                                     filter_contours_min_area,
                                     filter_contours_min_perimeter,
@@ -210,77 +188,50 @@ def detect_contours(image: np.ndarray, **kwargs) -> np.ndarray:
                                     filter_contours_min_vertices,
                                     filter_contours_min_ratio,
                                     filter_contours_max_ratio)
-        print(len(cnts))
         # loop over the contours
         screenCnt: np.ndarray = None
         contour: np.ndarray
         for contour in cnts:
-            # area = cv2.contourArea(contour)
             hull = cv2.convexHull(contour)
-            # hull_area = cv2.contourArea(hull)
-            # solidity = float(area)/hull_area
             contour = hull
-            # approximate the contour
             peri: int = cv2.arcLength(contour, True)
-            # approx: np.ndarray = cv2.approxPolyDP(contour, 0.007 * peri, True)
-            # approx: np.ndarray = cv2.approxPolyDP(contour, epsilon=0.05 * peri, closed=True)
             approx: np.ndarray = cv2.approxPolyDP(contour, epsilon=0.02 * peri, closed=True)
-            # print(len(approx))
-
-            # x,y,w,h = cv2.boundingRect(contour)
-            # area_bounding = w*h
-
-            # if solidity < 0.7:
-            #     print('SOLIDITY')
-            #     continue
-            # if area_bounding / area > 1.20:
-            #     print('AREA')
-            #     continue
+            
             if 4 <= len(approx) <= 4:
-                cv2.drawContours(output, [contour], -1, (0, 255, 0), 10)
+                contour = (contour * orig.shape[0]) // image_height_resize
+                cv2.drawContours(output, [contour], -1, (0, 255, 0), orig.shape[0]//200)
                 screenCnt: np.ndarray = approx
+                screenCnt = (screenCnt * orig.shape[0]) // image_height_resize
                 # (x,y),(MA,ma),angle = cv2.fitEllipse(contour)
-                # M = cv2.getRotationMatrix2D((image.shape[1] // 2, image.shape[0] // 2), -min(angle%90, abs(90-angle), abs(180-angle)), 1.0)
-                # image = cv2.warpAffine(image, M, (width, height))
                 found = True
-                # print('area_bounding / area: ' + str(area_bounding / area))
-                # print('solidity: ' + str(solidity))
                 output_contour = contour.copy()
-                print(tresh)
                 break
             elif len(approx) < 4 or len(approx) > 4:
-                cv2.drawContours(output, [contour], -1, (0, 0, 255), 10)
+                cv2.drawContours(output, [contour], -1, (0, 0, 255), orig.shape[0]//200)
         if found:
             break
     # show the contour (outline) of the piece of paper
-    print("STEP 2: Find contours of paper")
-    # cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
-    # cv2.imwrite('./src./images/Outline.png', image)
-
-    output: np.ndarray = imutils.resize(output, height = 400)
-    edged: np.ndarray = imutils.resize(edged, height = 400)
-    gray: np.ndarray = imutils.resize(gray, height = 400)
+    # print("STEP 2: Find contours of paper")
+    # output: np.ndarray = imutils.resize(output, height = 800)
+    # edged: np.ndarray = imutils.resize(edged, height = 800)
+    # gray: np.ndarray = imutils.resize(gray, height = 800)
     return output_contour #, output, edged
+
     if screenCnt is None:
-        return output_contour #, output, edged
+        return output_contour , output, edged
 
     # apply the four point transform to obtain a top-down
     # view of the original image
-    # orig = imutils.resize(orig, height = 400)
-    # screenCnt *= orig.shape[0] // output.shape[0]
-    warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
-    # convert the warped image to grayscale, then threshold it
-    # to give it that 'black and white' paper effect
+    warped = four_point_transform(orig, screenCnt.reshape(4, 2))
+    
     warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
     T = threshold_local(warped, 11, offset = 10, method = "gaussian")
     warped = (warped > T).astype("uint8") * 255
-    # show the original and scanned images
-    print("STEP 3: Apply perspective transform")
-    # cv2.imwrite('./src./images/Original.png', orig)
-    # cv2.imwrite('./src./images/Scanned.png', warped)
+    
+    # print("STEP 3: Apply perspective transform")
 
-    warped: np.ndarray = imutils.resize(warped, height = 400)
-    return output_contour #, output, warped
+    warped: np.ndarray = imutils.resize(warped, height = 800)
+    return output_contour , output, warped
 
 
 if __name__ == '__main__':
@@ -297,19 +248,19 @@ if __name__ == '__main__':
     win = Tk()
 
     # Set the size of the window
-    win.geometry("1280x720")
+    win.geometry("1600x900")
 
     # Create a Label to capture the Video frames
     label =Label(win)
     label2 =Label(win)
     label.grid(row=0, column=0)
     label2.grid(row=0, column=1)
-    capture: cv2.VideoCapture = cv2.VideoCapture(0)
-    width = 1280
-    height = 720
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-    print(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+    # capture: cv2.VideoCapture = cv2.VideoCapture(0)
+    # width = 1600
+    # height = 900
+    # capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    # capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    # print(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 
     # get the path/directory
     folder_dir = "C:/Users/Vince/OneDrive/Bureau/University/GIF/Session 7/Vision/Projet/images"
@@ -321,7 +272,7 @@ if __name__ == '__main__':
 
     def show_frames(counter: int) -> None:
         # Get the latest frame and convert into Image
-        img: np.ndarray = capture.read()[1]
+        # img: np.ndarray = capture.read()[1]
         if counter >= len(image_dir):
             counter = 0
         img: np.ndarray = cv2.imread(image_dir[counter])
@@ -344,7 +295,7 @@ if __name__ == '__main__':
         label2.imgtk = edgedtk
         label2.configure(image=edgedtk)
         # Repeat after an interval to capture continiously
-        label.after(500, lambda: show_frames(counter + 1))
+        label.after(1000, lambda: show_frames(counter + 1))
 
 
     show_frames(0)
